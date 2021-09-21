e0e5ffaca32b709ff68b5f95bb8a09b2f193a14e
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2021-09-21 09:27:36 +0200

    Update average.py

--- average.py
+++ average.py
@@ -1,9 +1,20 @@
-a = int(input("amount of nums"))
-x = 0 
-for i in range (0,a):
-  num = int(input("number"))
-  x=x+num 
+#include <iostream>
 
-x=x/a
-print(x)
-  
+int main() {
+    
+int a ;
+std::cout<<"amount of nums";
+std::cin>>a;
+int num;
+int x;
+
+while (a>0)
+{
+std::cin>>num;
+x=x+num;
+}
+x-x/a;
+std::cout<<x<<std::endl;
+
+    return 0;
+}
