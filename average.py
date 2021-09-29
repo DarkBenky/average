09ed41b9bfc187c06e015196e5c732a70161db93
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2021-09-29 10:23:03 +0200

    Update average.py

--- average.py
+++ average.py
@@ -1,8 +1,27 @@
-a = float(input())
-b = float(input())
-c = float(input())
-x = float
-x=a+b+c
-x=x/3
-
-print (x)
+a = 1
+x = 1
+y = 1
+w = int(input('width'))
+h = int(input('lenght'))
+r =[[0]*w for i in range(0,h)]
+while a == 1:
+    for riadok in r:
+        print(riadok)
+    print('hrac1') 
+    x=int(input('x'))
+    y=int(input('y'))
+    if r[x][y]==0:
+        r[x][y]=1
+    for riadok in r:
+        print(riadok)
+    print('hrac2')   
+    x=int(input('x'))
+    y=int(input('y'))
+    if r[x][y]==0:
+        r[x][y]=2
+    for riadok in r:
+        cislo = 3
+        pocet = 0
+        
+         
+        
