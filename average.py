04208c587cfcd36ccd982b97d381789e1159726d
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2021-11-03 09:27:52 +0100

    Update average.py

--- average.py
+++ average.py
@@ -1,8 +1,31 @@
-a = float(input())
-b = float(input())
-c = float(input())
-x = float
-x=a+b+c
-x=x/3
+from typing import List
 
-print (x)
+str='''gfhfjh ghfjhfj
+hmjgjg gfhfjh ghfjhfj
+hmjgjg gfhfjh ghfjhfj
+hmjgjg'''
+
+
+d={}
+for i in str:
+    if i in d:
+        d[i]=d[i]+1
+    else :
+        d[i]=1 
+print (d)        
+a = 1
+
+for i in str: 
+    if i == ' ':
+        a = a + 1
+print ('\n')
+
+boha = 1
+for i in str: 
+    if i == '\n':
+        boha = boha + 1
+        a = a + 1
+print ('\n')
+print ('riadkov :' ,boha)
+print ('\n')
+print ('slov :' , a)
