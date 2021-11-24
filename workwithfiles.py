a1c5e63dab33993f5c4d56dffb956a3dad690b26
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2021-11-24 10:11:18 +0100

    Update workwithfiles.py

--- workwithfiles.py
+++ workwithfiles.py
@@ -3,12 +3,14 @@ f=open("OpenData_Slovakia_Covid_PositiveTests_AgeGroup_District_mini.csv",encodi
 lines=f.readlines()
 m = 0
 z = 0
-mesta=[]
-pocet=[]
+pohlavie = {}
+
 mesto ={}
 max = 0
 loc = "a"
 vek={}
+vekkat =''
+pocet = 0
 for a in lines:
     a = a.rstrip() #  rozdeli na riatky stringov
     data = a.split(";") #  rozdeli na pole delen ;
@@ -18,7 +20,7 @@ for a in lines:
         mesto[data[3]]=1 
 print (mesto)
 for k , v in mesto.items():
-    print (k,v)
+
     if v > max:
         max = v
         loc = k
@@ -29,13 +31,28 @@ print ("najviac nakazenich mesto ",max,loc)
 for z in lines:
     z = z.rstrip() #  rozdeli na riatky stringov
     data = z.split(";") #  rozdeli na pole delen ;
+    if data[2] in pohlavie:
+        pohlavie[data[2]]+=1
+    else:
+        pohlavie[data[2]]=1
+print (pohlavie)
+
+for z in lines:
+    z = z.rstrip() #  rozdeli na riatky stringov
+    data = z.split(";") #  rozdeli na pole delen ;
     if data[4] in vek:
         vek[data[4]]+=1
     else:
         vek[data[4]]=1
 print (vek)
 
-
+for x , y in vek.items():
+    if y > pocet:
+        pocet = y
+        vekkat = x
+    else :
+        continue
+print ("najviacia vekova kategoria je ",vekkat,pocet)
 
 # for x,y in enumerate(lines):
 #     if x < 100:
