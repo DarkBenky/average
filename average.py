a930662a5cd4d503d48f9f404f46bf27572d3f79
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2022-01-27 10:23:50 +0100

    Update average.py

--- average.py
+++ average.py
@@ -1,10 +1,11 @@
-arr = [32, 67, 88, 13, 50]
-max = 0
-min = 100
 
-for i in arr:
-    if i > max:
-        max = i
-    if i < min:
-        min = i
-print (min, max)
+arr = [32, 67, 88, 13, 50]
+
+for i in range(len(arr)-1,0,-1):
+    for j in range(i):
+        if arr[j] == arr[j+1]:
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+
+print(arr)
