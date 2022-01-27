d3dde32032b4dbb92b7ebd4c1aa3c6a96fc5a229
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2022-01-27 10:14:50 +0100

    Update average.py

--- average.py
+++ average.py
@@ -1,65 +1,10 @@
-def rozbit():
+arr = [32, 67, 88, 13, 50]
+max = 0
+min = 100
 
-    x = '10*5/4+8+20*4'
-    
-    nums = []
-    znaminka=[]
-    last_index=-1
-
-    for i , c in enumerate(x):
-        if c =='+' or c =='-':
-            nums.append(x[1+last_index:i])
-            znaminka.append(c)
-            last_index = i
-
-    nums.append(x[1+last_index:])
-    return nums,znaminka
-
-def roz(x):
-
-    nums = []
-    znaminka=[]
-    last_index=-1
-
-    for i , c in enumerate(x):
-        if c =='*' or c =='/':
-            nums.append(x[1+last_index:i])
-            znaminka.append(c)
-            last_index = i
-
-    nums.append(x[1+last_index:])
-    return nums,znaminka
-
-def zrataj(cisla,znaminka):
-    print (cisla , znaminka)
-    x = 0
-    lol = 0
-    z2=0
-    res = []
-    for i in znaminka:
-        z=int(cisla[x])
-        z1=int(cisla[x+1])
-        q=znaminka[x]
-        if q == '*':
-            z2 = z  * z1
-            del cisla[x]
-            x = x+1;
-            cisla.insert(x,z2)
-        if q == '/':
-            z2 = z  / z1
-            del cisla[x]
-            x = x+1;
-            cisla.insert(x,z2)
-    print (z2)
-    
-
-
-
-
-
-
-x , y = rozbit()
-print (x , y)
-for i in x:
-    x1 , y1 = roz (i)
-    zrataj (x1 , y1)
+for i in arr:
+    if i > max:
+        max = i
+    if i < min:
+        min = i
+print (min, max)
