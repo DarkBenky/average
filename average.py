2882a88f475ca5473534fd32cbe0a6ea658b95d4
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2022-02-01 21:01:34 +0100

    Update average.py

--- average.py
+++ average.py
@@ -1,74 +1,26 @@
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+def set(key, value):
+    global car
+    car[key] = value
+    print(car)
 
-import tkinter
-import random
+def check():
+    global car
+    _MAX_LENGTH = 4
+    length = len(car)
+    if length == _MAX_LENGTH:
+        for key in car:
+            car.pop(key)
+            break
+        print(car)
 
-def uloha():
-    x = ''
-    x1 = '*'
-    for i in range(0,10):
-        x = x + x1
-    print(x)
-    print("SPSE")
-    print("Zochova")
-    print (x)
+set("logo","BMW")
+check()
+set("logo1","BMW1")
 
-
-def uloha2():
-    x3=tkinter.Canvas(bg='black',width = 400 , height = 400)
-    x3.pack()
-
-    x = random.randint(100,300)
-    y = random.randint(100,300)
-
-    x1 = random.randint(100,300)
-    y1 = random.randint(100,300)
-
-
-    x3.create_text(x,y,text='SPSE',fill='gold')
-    x3.create_text(x1,y1,text='Zochova',fill='gold')
-    x3.mainloop()
-
-
-
-def uloha3(x):
-    z =''
-    for i in range(0,x):
-        z = z + "@"
-    print (z)
-
-
-x = int(input('Enter'))
-uloha3(x)
-
-
-def uloha4(x):
-    x1= "*"
-    for i in range(0,x):
-        print(x1)
-        x1 = x1 + "*"
-
-uloha4(x)
-
-def uloha5(x,z):
-    x3=tkinter.Canvas(bg='black',width = 400 , height = 400)
-    x3.pack()
-
-    
-    
-
-    
-    for i in range(0,x+1):
-        x1 = random.randint(20,380)
-        y1 = random.randint(20,380)
-
-        x = random.randint(20,380)
-        y = random.randint(20,380)
-
-        
-        x3.create_rectangle(x1,y1,x,y,fill=z)
-
-    x3.mainloop()    
-
-z = input("color")
 
 uloha5(x,z)
