44e5580224243e1e6e0ebcf3c166a2ff3720c869
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2022-02-01 14:25:47 +0100

    Update average.py

--- average.py
+++ average.py
@@ -1,74 +1,144 @@
+import os
+from tkinter import *
+from tkinter import filedialog , colorchooser , font
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+from turtle import window_height
 
-import tkinter
-import random
+def change_color():
+    color = colorchooser.askcolor(title="pick color")
+    text_area.config(fg = color[1])
 
-def uloha():
-    x = ''
-    x1 = '*'
-    for i in range(0,10):
-        x = x + x1
-    print(x)
-    print("SPSE")
-    print("Zochova")
-    print (x)
+def change_background():
+    color = colorchooser.askcolor(title="pick color")
+    text_area.config(bg = color[1])
 
+def change_font(*args):
+    text_area.config(font = (font_name.get() , size_box.get()))
 
-def uloha2():
-    x3=tkinter.Canvas(bg='black',width = 400 , height = 400)
-    x3.pack()
+def new_file():
+    window.title("Untitled")
+    text_area.delete(1.0,END)
 
-    x = random.randint(100,300)
-    y = random.randint(100,300)
+def save_file():
+    file = filedialog.asksaveasfilename(initialfile="Untitled.txt",
+                                        defaultextension = ".txt",
+                                        filetypes=[("ALL files", "*.*"),("Text Documents" , "*.txt"),]
+                                        )
+    if file == None:
+        return
+    else:
+        try:
+            window.title(os.path.basename(file))
+            file = open(file, "w")
+            
+            file.write(text_area.get(1.0,END))
+        except Exception:
+            print("couldn't save file")
+        finally:
+            file.close()
+def open_file():
+    file = askopenfilename(defaultextension=".txt",
+                           file=[("All Files", "*.*"),
+                                 ("Text Documents", "*.txt")])
 
-    x1 = random.randint(100,300)
-    y1 = random.randint(100,300)
+    if file is None:
+        return
 
+    else:
+        try:
+            window.title(os.path.basename(file))
+            text_area.delete(1.0, END)
 
-    x3.create_text(x,y,text='SPSE',fill='gold')
-    x3.create_text(x1,y1,text='Zochova',fill='gold')
-    x3.mainloop()
+            file = open(file, "r")
 
+            text_area.insert(1.0, file.read())
 
+        except Exception:
+            print("couldn't read file")
 
-def uloha3(x):
-    z =''
-    for i in range(0,x):
-        z = z + "@"
-    print (z)
-
-
-x = int(input('Enter'))
-uloha3(x)
-
-
-def uloha4(x):
-    x1= "*"
-    for i in range(0,x):
-        print(x1)
-        x1 = x1 + "*"
-
-uloha4(x)
-
-def uloha5(x,z):
-    x3=tkinter.Canvas(bg='black',width = 400 , height = 400)
-    x3.pack()
-
-    
-    
-
-    
-    for i in range(0,x+1):
-        x1 = random.randint(20,380)
-        y1 = random.randint(20,380)
-
-        x = random.randint(20,380)
-        y = random.randint(20,380)
-
+        finally:
+            file.close()
         
-        x3.create_rectangle(x1,y1,x,y,fill=z)
+def cut():
+    text_area.event_generate("<<Cut>>")
 
-    x3.mainloop()    
+def copy():
+    text_area.event_generate("<<Copy>>")
 
-z = input("color")
+def paste():
+    text_area.event_generate("<<Paste>>")
 
-uloha5(x,z)
+def About():
+    showinfo("About this application" , "This program wrote DarkBenky" )
+
+def quit():
+    window.destroy()
+
+window = Tk()
+window.title("Text editor")
+file = None
+
+window_height =500
+window_width =500
+screen_width = window.winfo_screenmmwidth()
+screen_height = window.winfo_screenmmheight()
+
+x = int((screen_width / 1) - (window_width /3 ))
+y = int((screen_height / 1) - (window_height /3 ))
+
+window.geometry("{}x{}+{}-{}".format(window_width,window_height,x,y))
+
+font_name = StringVar(window)
+font_name.set("consolas")
+
+font_size = StringVar(window)
+font_size.set("25")
+
+text_area = Text(window,font=(font_name.get(),font_size.get()))
+
+scroll_bar = Scrollbar(text_area)
+
+window.grid_rowconfigure(0,weight=1)
+window.grid_columnconfigure(0,weight=1)
+
+text_area.grid(sticky= N + E + S + W)
+
+frame = Frame(window)
+frame.grid()
+
+color_button = Button(frame,text = "Color",command = change_color)
+color_button.grid(row = 0 , column = 0)
+background_button = Button(frame,text = "Background",command = change_background)
+background_button.grid(row = 0 , column = 1)
+font_box = OptionMenu (frame,font_name,*font.families(),command =change_font)
+font_box.grid(row = 0 , column =2)
+size_box = Spinbox(frame,from_=8,to = 100 , textvariable=font_size , command = change_font)
+size_box.grid(row = 0 , column =3)
+
+menu_bar = Menu(window)
+window.config(menu = menu_bar)
+
+file_menu = Menu(menu_bar ,tearoff=0)
+menu_bar.add_cascade(label = "File" , menu = file_menu)
+
+file_menu.add_command(label = "New" , command =new_file)
+file_menu.add_command(label ="Open" , command =open_file)
+file_menu.add_command(label ="Save" , command =save_file)
+file_menu.add_separator()
+file_menu.add_command(label ="Exit", command= quit)
+
+edit_menu = Menu(menu_bar,tearoff=0)
+menu_bar.add_cascade(label = "Edit" , menu = edit_menu)
+edit_menu.add_command(label = "Cut" , command =cut)
+edit_menu.add_command(label = "Copy" , command =copy)
+edit_menu.add_command(label = "Paste" , command =paste)
+
+help_menu = Menu(menu_bar,tearoff=0)
+menu_bar.add_cascade(label = "Help", menu = help_menu)
+help_menu. add_command(label = "About" , command = About)
+
+scroll_bar.pack(side="right",fill=Y)
+text_area.config(yscrollcommand=scroll_bar.set)
+
+window.mainloop()
