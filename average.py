9c8ab78012423419a9f0b954de986e74ef14e5cd
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2022-02-02 07:57:16 +0100

    Update average.py

--- average.py
+++ average.py
@@ -1,26 +1,41 @@
+import random
+
 car = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
+def check():
+    global car
+    _MAX_LENGTH = 2
+    length = len(car)
+    if length >= _MAX_LENGTH:
+        for key in car:
+            car.pop(key)
+            break
+        print(car)
+        
+        
+def check_all ():
+    global car 
+    MAX_LENGTH = 2
+    length = len(car)
+    MAX_LENGTH = length - MAX_LENGTH
+    for i in range(MAX_LENGTH):
+        check()
+        
 def set(key, value):
     global car
     car[key] = value
     print(car)
 
-def check():
-    global car
-    _MAX_LENGTH = 4
-    length = len(car)
-    if length == _MAX_LENGTH:
-        for key in car:
-            car.pop(key)
-            break
-        print(car)
 
-set("logo","BMW")
-check()
 set("logo1","BMW1")
+set("logo2","BMW2")
+set("logo3","BMW3")
+set("logo4","BMW4") 
+check_all()
+
 
 
 
