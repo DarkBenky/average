7f05db1f3c87d21914951822fe6fa24f59541f59
DarkBenky <43213759+DarkBenky@users.noreply.github.com>
2022-02-02 10:07:12 +0100

    Update average.py

--- average.py
+++ average.py
@@ -1,74 +1,33 @@
+from tkinter import*
 
-import tkinter
-import random
+window = Tk()
+window.title("Calculator")
+window.geometry("500x250")
 
-def uloha():
-    x = ''
-    x1 = '*'
-    for i in range(0,10):
-        x = x + x1
-    print(x)
-    print("SPSE")
-    print("Zochova")
-    print (x)
+result = StringVar()
 
-
-def uloha2():
-    x3=tkinter.Canvas(bg='black',width = 400 , height = 400)
-    x3.pack()
-
-    x = random.randint(100,300)
-    y = random.randint(100,300)
-
-    x1 = random.randint(100,300)
-    y1 = random.randint(100,300)
-
-
-    x3.create_text(x,y,text='SPSE',fill='gold')
-    x3.create_text(x1,y1,text='Zochova',fill='gold')
-    x3.mainloop()
-
-
-
-def uloha3(x):
-    z =''
-    for i in range(0,x):
-        z = z + "@"
-    print (z)
-
-
-x = int(input('Enter'))
-uloha3(x)
-
-
-def uloha4(x):
-    x1= "*"
-    for i in range(0,x):
-        print(x1)
-        x1 = x1 + "*"
-
-uloha4(x)
-
-def uloha5(x,z):
-    x3=tkinter.Canvas(bg='black',width = 400 , height = 400)
-    x3.pack()
-
-    
-    
-
-    
-    for i in range(0,x+1):
-        x1 = random.randint(20,380)
-        y1 = random.randint(20,380)
-
-        x = random.randint(20,380)
-        y = random.randint(20,380)
-
+def Calculate():
+    try:
+        Eqation = entry_eqation.get()
+        total = str(eval(Eqation))
+        print (total)
+        result.set(total)
+    except ZeroDivisionError:
+        result.set('Error')
+    except SyntaxError:
+        result.set('Error')
+    except NameError:
+        result.set('Error')
         
-        x3.create_rectangle(x1,y1,x,y,fill=z)
+        
+    
 
-    x3.mainloop()    
-
-z = input("color")
-
-uloha5(x,z)
+label = Label(window,text="Eqation",font=('Consolas',25))
+label.pack()
+entry_eqation = Entry(window,font=('Consolas',25))
+entry_eqation.pack()
+label = Label(window,textvariable=result,font=('Consolas',25),bg="white")
+label.pack()
+Calculate = Button(window,text="Calculate the equation",font=('Consolas',25),command=Calculate)
+Calculate.pack()
+window.mainloop()
